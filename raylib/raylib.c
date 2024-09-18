56e2e26e308665bba40b64a1b12c343d56a0104e
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2024-09-18 17:53:19 -0300

    refactorings

--- raylib/raylib.c
+++ raylib/raylib.c
@@ -4,39 +4,44 @@
 #include <math.h>
 #include <time.h>
 
-#define W 1024
-#define H 768
 #define RADIAN(x) (((x) * M_PI/30.0) - (M_PI/2.0))
 #define HOUR(x) (((x) * 5) % 60)
-#define HANDX(val, size) (W/2 + (size) * cos(RADIAN(val)))
-#define HANDY(val, size) (H/2 + (size) * sin(RADIAN(val)))
+#define HANDX(val, size) ((size) * cos(RADIAN(val)))
+#define HANDY(val, size) ((size) * sin(RADIAN(val)))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 int main(void) {
-  InitWindow(W, H, "Clocks");
-  float radius = 360.;
-  float hourHandSize = 180.;
-  float minuteHandSize = 200.;
-  float secondHandSize = 300.;  
+  InitWindow(1024, 768, "Clocks");
+  SetTargetFPS(10);
+  SetWindowState(FLAG_WINDOW_RESIZABLE);
   while (!WindowShouldClose()) {
     struct tm *timeinfo;
     time_t rawtime;
     time(&rawtime);
     timeinfo = localtime(&rawtime);
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
+    int centerX = screenWidth/2;
+    int centerY = screenHeight/2;
+    float radius = MIN(screenWidth, screenHeight)*0.3;
+    float hourHandSize = radius*0.5;
+    float minuteHandSize = radius*0.75;
+    float secondHandSize = radius*0.9;
     int hours = timeinfo->tm_hour;
     int minutes = timeinfo->tm_min;
     int seconds = timeinfo->tm_sec;
-    int endHourX = HANDX(HOUR(hours), hourHandSize);
-    int endHourY = HANDY(HOUR(hours), hourHandSize);
-    int endMinuteX = HANDX(minutes, minuteHandSize);
-    int endMinuteY = HANDY(minutes, minuteHandSize);
-    int endSecondX = HANDX(seconds, secondHandSize);
-    int endSecondY = HANDY(seconds, secondHandSize);
+    int endHourX = centerX + HANDX(HOUR(hours), hourHandSize);
+    int endHourY = centerY + HANDY(HOUR(hours), hourHandSize);
+    int endMinuteX = centerX + HANDX(minutes, minuteHandSize);
+    int endMinuteY = centerY + HANDY(minutes, minuteHandSize);
+    int endSecondX = centerX + HANDX(seconds, secondHandSize);
+    int endSecondY = centerY + HANDY(seconds, secondHandSize);
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawCircleLines(W/2, H/2, radius, BLACK);
-    DrawLine(W/2, H/2, endHourX, endHourY, BLACK);
-    DrawLine(W/2, H/2, endMinuteX, endMinuteY, LIGHTGRAY);
-    DrawLine(W/2, H/2, endSecondX, endSecondY, RED);
+    DrawCircleLines(centerX, centerY, radius, BLACK);
+    DrawLine(centerX, centerY, endHourX, endHourY, BLACK);
+    DrawLine(centerX, centerY, endMinuteX, endMinuteY, LIGHTGRAY);
+    DrawLine(centerX, centerY, endSecondX, endSecondY, RED);
     EndDrawing();
   }
   CloseWindow();
