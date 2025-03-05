e21791d09a7d91eaf5ef80e9b411b33ca2ec9bb7
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2025-03-05 12:06:13 -0300

    update tk clock

--- clock.tcl
+++ clock.tcl
@@ -1,81 +1,107 @@
-# This program draws an analogical clock 
-# author: Humberto Henrique Campos Pinheiro
-# created: 18/07/13
-# Run: wish clock.tcl
+#!/usr/bin/env tclsh
 
-# configure window
-wm title . TkClock
-wm geometry . 250x250+0+0
-frame .fr
-pack .fr -fill both -expand 1
+package require Tk
 
-# draw the clock
-canvas .fr.can
-pack .fr.can -side top -fill both -expand 1
-set wall [expr min([.fr.can cget -width], [.fr.can cget -height])-50]
-set centerX 120
-set centerY 120
-set radius 90
-.fr.can create oval 20 20 $wall $wall -fill #000
+# Create the main window
+wm title . "Analog Clock"
 
-# map a number from 0-59 to radians
-proc toRadian {x} {
-    set pi 3.1415
-    #assure that $x is not interpreted as octal
-    regexp {^0*(\d+)} $x _dummy x
-    expr ($x * $pi/30) - ($pi/2)
+canvas .canvas -width 300 -height 300
+pack .canvas -fill both -expand true
+
+# Define the center of the clock
+set centerX 150
+set centerY 150
+set radius 120
+
+# Function to draw the clock face
+proc draw_clock {} {
+    global centerX centerY radius
+    .canvas delete all
+
+    # Draw clock circle
+    .canvas create oval [expr $centerX - $radius] [expr $centerY - $radius] \
+        [expr $centerX + $radius] [expr $centerY + $radius] \
+        -outline "black" -width 2
+
+    # Draw clock ticks
+    for {set i 0} {$i < 12} {incr i} {
+        set angle [expr $i * 30]
+        set x1 [expr $centerX + cos($angle * 3.14159 / 180) * ($radius - 10)]
+        set y1 [expr $centerY - sin($angle * 3.14159 / 180) * ($radius - 10)]
+        set x2 [expr $centerX + cos($angle * 3.14159 / 180) * $radius]
+        set y2 [expr $centerY - sin($angle * 3.14159 / 180) * $radius]
+        .canvas create line $x1 $y1 $x2 $y2 -fill "black"
+    }
 }
 
-# return the coordinates of the clock hands based in current time
-proc hourX {centerX radius} {
-    set hour [expr [clock format [clock seconds] -format {%l}] * 5]
-    expr $centerX + 0.4 * $radius * cos([toRadian $hour])
+# Function to update the hands of the clock
+proc update_clock {} {
+    global centerX centerY radius
+
+    # Get the current time
+    set time [clock seconds]
+    set hours [clock format $time -format "%I"]
+    set minutes [clock format $time -format "%M"]
+    set seconds [clock format $time -format "%S"]
+
+    # Convert to angles
+    set hour_angle [expr {($hours % 12) * 30 + ($minutes / 60.0) * 30}]
+    set minute_angle [expr {$minutes * 6}]
+    set second_angle [expr {$seconds * 6}]
+
+    # Clear previous hands
+    .canvas delete hands
+
+    # Draw hands
+    .canvas create line $centerX $centerY \
+        [expr $centerX + cos(($hour_angle - 90) * 3.14159 / 180) * ($radius * 0.5)] \
+        [expr $centerY + sin(($hour_angle - 90) * 3.14159 / 180) * ($radius * 0.5)] \
+        -fill "black" -width 6 -tags hands
+
+    .canvas create line $centerX $centerY \
+        [expr $centerX + cos(($minute_angle - 90) * 3.14159 / 180) * ($radius * 0.8)] \
+        [expr $centerY + sin(($minute_angle - 90) * 3.14159 / 180) * ($radius * 0.8)] \
+        -fill "blue" -width 4 -tags hands
+
+    .canvas create line $centerX $centerY \
+        [expr $centerX + cos(($second_angle - 90) * 3.14159 / 180) * ($radius * 0.9)] \
+        [expr $centerY + sin(($second_angle - 90) * 3.14159 / 180) * ($radius * 0.9)] \
+        -fill "red" -width 2 -tags hands
 }
 
-proc hourY {centerY radius} {
-    set hour [expr [clock format [clock seconds] -format {%l}] * 5]
-    expr $centerY + 0.4 * $radius * sin([toRadian $hour])
+# Function to update the clock every second
+proc update_time {} {
+    update_clock
+    after 1000 update_time
 }
 
-proc minuteX {centerX radius} {
-    set minute [clock format [clock seconds] -format {%M}]
-    expr $centerX + 0.7 * $radius * cos([toRadian $minute])
+proc resize_clock {w h} {
+    global centerX centerY radius
+
+    # Set new center and radius based on canvas size
+    set centerX [expr $w / 2]
+    set centerY [expr $h / 2]
+    set radius [expr [min $w $h] / 2.5]
+
+    # Redraw everything
+    draw_clock
+    update_clock
 }
 
-proc minuteY {centerY radius} {
-    set minute [clock format [clock seconds] -format {%M}]
-    expr $centerY + 0.7 * $radius * sin([toRadian $minute])
+# Helper function to find the minimum of two numbers
+proc min {a b} {
+    if {$a < $b} {
+        return $a
+    } else {
+        return $b
+    }
 }
 
-proc secondX {centerX radius} {
-    set second [clock format [clock seconds] -format {%S}]
-    expr $centerX + $radius * cos([toRadian $second])
-}
+# Bind resize event to update clock
+bind .canvas <Configure> {resize_clock %w %h}
 
-proc secondY {centerY radius} {
-    set second [clock format [clock seconds] -format {%S}]
-    expr $centerY + $radius * sin([toRadian $second])
-}
 
-set hourHand [.fr.can create line $centerX $centerY [hourX $centerX $radius] \
-[hourY $centerY $radius] -fill red]
+# Draw clock face and start the time updates
+draw_clock
+update_time
 
-set minuteHand [.fr.can create line $centerX $centerY [minuteX $centerX $radius] \
-[minuteY $centerY $radius] -fill red]
-
-set secondHand [.fr.can create line $centerX $centerY [secondX $centerX $radius] \
-[secondY $centerY $radius] -fill green]
-
-# update every second
-proc tick {} {
-    global hourHand minuteHand secondHand centerX centerY radius
-    .fr.can coords $hourHand $centerX $centerY [hourX $centerX $radius] \
-        [hourY $centerY $radius]
-    .fr.can coords $minuteHand $centerX $centerY [minuteX $centerX $radius] \
-        [minuteY $centerY $radius]
-    .fr.can coords $secondHand $centerX $centerY [secondX $centerX $radius] \
-        [secondY $centerY $radius]
-    after 1000 tick
-    return
-}
-tick
