2b319ec9fa84cb73b17a42f38907670ac52f1026
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2026-03-03 12:17:36 -0300

    remove warning gtk

--- gtk-clock/gtk3.c
+++ gtk-clock/gtk3.c
@@ -24,7 +24,7 @@ int main (int argc, char **argv)
 
   update_time();
 
-	app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 	status = g_application_run (G_APPLICATION (app), argc, argv);
 	g_object_unref (app);
